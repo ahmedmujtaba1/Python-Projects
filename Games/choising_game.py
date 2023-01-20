64c32ef981ec5c6ae381a72698aa4103206aa102
★彡[ᴀʜᴍᴇᴅ ᴍᴜᴊᴛᴀʙᴀ]彡★ <107427098+ahmedmujtaba1@users.noreply.github.com>
2023-01-20 18:57:40 +0500

    Add files via upload

---
+++ Games/choising_game.py
@@ -0,0 +1,91 @@
+from tkinter import *
+import random
+
+ws = Tk()
+ws.title('Python-Choosing Game')
+ws.geometry('600x400')
+ws.config(bg='#5671A6')
+
+ranNum = random.randint(0, 100)
+chance = 10
+var = IntVar()
+disp = StringVar()
+
+def check_guess():
+    global ranNum
+    global chance
+    usr_ip = var.get()
+    if chance > 0:
+        if usr_ip == ranNum:
+            msg = f'You won! {ranNum} is the right answer.'
+        elif usr_ip > ranNum:
+            chance -= 1
+            msg = f'{usr_ip} is greater. You have {chance} attempt left.'
+        elif usr_ip < ranNum:
+            chance -= 1
+            msg = f'{usr_ip} is smaller. You have {chance} attempt left.'
+        else:
+            msg = 'Something went wrong!'
+    else:
+        msg = f'You Lost! you have {chance} attempt left.'
+        Label(
+            ws,
+            text=f"The number is {ranNum}",
+            relief=SOLID,
+            padx=10,
+            pady=10,
+            bg='#F27D16'
+        ).pack(pady=(10, 0))
+        
+    disp.set(msg)
+
+
+Label(
+    ws,
+    text='Number Guessing Game',
+    font=('sans-serif', 20),
+    relief=SOLID,
+    padx=10,
+    pady=10,
+    bg='#F27D16'
+).pack(pady=(10, 0))
+
+Label(
+    ws,
+    text='Guess Number 0-100',
+    font=('sans-serif', 20),
+    relief=SOLID,
+    padx=3,
+    pady=4,
+    bg='#FFA07A'
+).pack(pady=(10, 0))
+
+Entry(
+    ws,
+    textvariable=var,
+    font=('sans-serif', 18)
+).pack(pady=(50, 10))
+
+Button(
+    ws,
+    text='Submit Guess',
+    font=('sans-serif', 18),
+    command=check_guess
+).pack()
+
+Label(
+    ws,
+    textvariable=disp,
+    bg='#5671A6',
+    font=('sans-serif', 14)
+).pack(pady=(20,0))
+
+Label(
+    ws,
+    text="Made By Ahmed Mujtaba | 12 years old!",
+    font=('sans-serif', 13),
+    relief=SOLID,
+    bg='#FFA07A'
+).pack(pady=(3, 0))
+
+ws.mainloop()
